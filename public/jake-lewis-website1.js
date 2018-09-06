83572059e6b711329b8056b6130539c85433a7f8
Jake Lewis <jakeaustinlewis@gmail.com>
2018-09-05 23:20:49 -0500

    changed the layout of the mobile navbar

--- public/jake-lewis-website1.js
+++ public/jake-lewis-website1.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             main.addEventListener('click', (e) => {
                 sidebar.className = 'slideIn';
+                
             })
         }
     });
